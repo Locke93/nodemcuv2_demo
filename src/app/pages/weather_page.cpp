a66a10544dfc9cb90debceb4c6959e44fee04671
Locke Peng <locke.peng@ringcentral.com>
2022-02-27 16:53:34 +0800

    feat: add icon for different weather

--- src/app/pages/weather_page.cpp
+++ src/app/pages/weather_page.cpp
@@ -6,6 +6,7 @@
 #include "../network/http_request.h"
 #include "../network/http_manager.h"
 #include "../local_properties.h"
+#include "../resources.h"
 
 WeatherPage::~WeatherPage() {
     ticker.detach();
@@ -14,21 +15,20 @@ WeatherPage::~WeatherPage() {
 void WeatherPage::onCreate() {
     std::shared_ptr<ViewGroup> rooView = std::make_shared<ViewGroup>();
 
-    textView = std::make_shared<TextView>();
-    textView->setText("...");
-    textView->setPosition(0, 20);
-    textView->setFont(u8g2_font_8x13_tr);
-    rooView->addView(textView);
+    iconView = std::make_shared<ImageView>();
+    iconView->setPosition(0, 0);
+    iconView->setImage(74, 32, weather_icon_99);
+    rooView->addView(iconView);
 
     temView = std::make_shared<TextView>();
     temView->setPosition(80, 28);
     temView->setFont(u8g2_font_10x20_tr);
     rooView->addView(temView);
 
-    cityView = std::make_shared<TextView>();
-    cityView->setPosition(80, 8);
-    cityView->setFont(u8g2_font_5x7_tr);
-    rooView->addView(cityView);
+    textView = std::make_shared<TextView>();
+    textView->setPosition(80, 8);
+    textView->setFont(u8g2_font_5x7_tr);
+    rooView->addView(textView);
     setContentView(std::static_pointer_cast<View>(rooView));
 
     ticker.attach(60 * 15, std::bind(&WeatherPage::requestWeather, this));
@@ -49,18 +49,140 @@ void WeatherPage::requestWeather() {
         std::string code = getValueFromJson(response, "code");
         std::string temperature = getValueFromJson(response, "temperature");
 
+        auto icon = getIconByCode(code);
+        iconView->setImage(icon.first.first, icon.first.second, icon.second);
         textView->setText(text);
-        temView->setText(temperature);
-        cityView->setText(city);
+        temView->setText(temperature + "'C");
     };
     HttpManager::getInstance()->addRequest(*request, callback);
 }
 
+std::pair<std::pair<int, int>, const uint8_t *> WeatherPage::getIconByCode(const std::string &code) {
+    if (code == "0") {
+        return std::make_pair(std::make_pair(32, 32), weather_icon_0);
+    }
+    if (code == "1") {
+        return std::make_pair(std::make_pair(31, 32), weather_icon_1);
+    }
+    if (code == "2") {
+        return std::make_pair(std::make_pair(32, 32), weather_icon_2);
+    }
+    if (code == "3") {
+        return std::make_pair(std::make_pair(31, 32), weather_icon_3);
+    }
+    if (code == "4") {
+        return std::make_pair(std::make_pair(41, 32), weather_icon_4);
+    }
+    if (code == "5") {
+        return std::make_pair(std::make_pair(44, 32), weather_icon_5);
+    }
+    if (code == "6") {
+        return std::make_pair(std::make_pair(38, 32), weather_icon_6);
+    }
+    if (code == "7") {
+        return std::make_pair(std::make_pair(46, 32), weather_icon_7);
+    }
+    if (code == "8") {
+        return std::make_pair(std::make_pair(37, 32), weather_icon_8);
+    }
+    if (code == "9") {
+        return std::make_pair(std::make_pair(48, 32), weather_icon_9);
+    }
+    if (code == "10") {
+        return std::make_pair(std::make_pair(33, 32), weather_icon_10);
+    }
+    if (code == "11") {
+        return std::make_pair(std::make_pair(32, 32), weather_icon_11);
+    }
+    if (code == "12") {
+        return std::make_pair(std::make_pair(32, 32), weather_icon_12);
+    }
+    if (code == "13") {
+        return std::make_pair(std::make_pair(33, 32), weather_icon_13);
+    }
+    if (code == "14") {
+        return std::make_pair(std::make_pair(33, 32), weather_icon_14);
+    }
+    if (code == "15") {
+        return std::make_pair(std::make_pair(33, 32), weather_icon_15);
+    }
+    if (code == "16") {
+        return std::make_pair(std::make_pair(33, 32), weather_icon_16);
+    }
+    if (code == "17") {
+        return std::make_pair(std::make_pair(34, 32), weather_icon_17);
+    }
+    if (code == "18") {
+        return std::make_pair(std::make_pair(34, 32), weather_icon_18);
+    }
+    if (code == "19") {
+        return std::make_pair(std::make_pair(31, 32), weather_icon_19);
+    }
+    if (code == "20") {
+        return std::make_pair(std::make_pair(33, 32), weather_icon_20);
+    }
+    if (code == "21") {
+        return std::make_pair(std::make_pair(32, 32), weather_icon_21);
+    }
+    if (code == "22") {
+        return std::make_pair(std::make_pair(34, 32), weather_icon_22);
+    }
+    if (code == "23") {
+        return std::make_pair(std::make_pair(34, 32), weather_icon_23);
+    }
+    if (code == "24") {
+        return std::make_pair(std::make_pair(34, 32), weather_icon_24);
+    }
+    if (code == "25") {
+        return std::make_pair(std::make_pair(32, 32), weather_icon_25);
+    }
+    if (code == "26") {
+        return std::make_pair(std::make_pair(38, 32), weather_icon_26);
+    }
+    if (code == "27") {
+        return std::make_pair(std::make_pair(38, 32), weather_icon_27);
+    }
+    if (code == "28") {
+        return std::make_pair(std::make_pair(55, 32), weather_icon_28);
+    }
+    if (code == "29") {
+        return std::make_pair(std::make_pair(55, 32), weather_icon_29);
+    }
+    if (code == "30") {
+        return std::make_pair(std::make_pair(35, 32), weather_icon_30);
+    }
+    if (code == "31") {
+        return std::make_pair(std::make_pair(36, 32), weather_icon_31);
+    }
+    if (code == "32") {
+        return std::make_pair(std::make_pair(41, 32), weather_icon_32);
+    }
+    if (code == "33") {
+        return std::make_pair(std::make_pair(41, 32), weather_icon_33);
+    }
+    if (code == "34") {
+        return std::make_pair(std::make_pair(32, 32), weather_icon_34);
+    }
+    if (code == "35") {
+        return std::make_pair(std::make_pair(32, 32), weather_icon_35);
+    }
+    if (code == "36") {
+        return std::make_pair(std::make_pair(33, 32), weather_icon_36);
+    }
+    if (code == "37") {
+        return std::make_pair(std::make_pair(28, 32), weather_icon_37);
+    }
+    if (code == "38") {
+        return std::make_pair(std::make_pair(32, 32), weather_icon_38);
+    }
+    return std::make_pair(std::make_pair(74, 32), weather_icon_99);
+}
+
 std::string WeatherPage::getValueFromJson(const std::string &json, const std::string &key) {
     int begin = json.find(key + "\":\"");
-    if (begin < 0) return "";
+    if (begin == std::string::npos) return "";
     begin += key.length() + 3;
     int end = json.find("\"", begin);
-    if (end < 0 || end <= begin) return "";
+    if (end == std::string::npos || end <= begin) return "";
     return json.substr(begin, end - begin);
 }
--- src/app/pages/weather_page.h
+++ src/app/pages/weather_page.h
@@ -7,6 +7,7 @@
 #include <memory>
 #include "../screen_page.h"
 #include "../../view/text_view.h"
+#include "../../view/image_view.h"
 
 class WeatherPage : public ScreenPage {
 public:
@@ -17,11 +18,13 @@ public:
 private:
     void requestWeather();
 
+    std::pair<std::pair<int, int>, const uint8_t *> getIconByCode(const std::string &code);
+
     std::string getValueFromJson(const std::string &json, const std::string &key);
 
 private:
-    std::shared_ptr<TextView> textView;
+    std::shared_ptr<ImageView> iconView;
     std::shared_ptr<TextView> temView;
-    std::shared_ptr<TextView> cityView;
+    std::shared_ptr<TextView> textView;
     Ticker ticker;
 };
